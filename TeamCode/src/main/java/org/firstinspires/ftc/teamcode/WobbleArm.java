56b9bae3a8eda68817abb6337e0a473bfa6bc3b9
metalworks <github@ftc.rocks>
2020-09-27 13:37:49 -0400

    This moves the arm with a motor across the frame with a servo to hold onto the top of the Wobble.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WobbleArm.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "WobbleArm")
+
+public class WobbleArm extends OpMode {
+    DcMotor motor;
+    Servo servo;
+    @Override
+    public void init() {
+
+        motor = hardwareMap.dcMotor.get("m");
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        servo = hardwareMap.servo.get("s");
+    }
+
+    @Override
+    public void loop() {
+
+        motor.setPower(gamepad1.left_stick_x*0.5);
+        servo.setPosition(1-gamepad1.left_trigger);
+    }
+}
