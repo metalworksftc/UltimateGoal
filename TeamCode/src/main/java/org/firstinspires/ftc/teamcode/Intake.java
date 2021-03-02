082973b987b69e9a089ded9f2346b3efa9e62a33
metalworks <github@ftc.rocks>
2021-03-01 19:11:20 -0500

    Disabled PID

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -40,45 +40,52 @@ public class Intake {
 
     public void flywheel(boolean on) {
         final long time = System.currentTimeMillis() - formerTime;
-            if (time > 100) {
-                final int counts = launchMotor.getCurrentPosition() - formerCounts;
-                formerCounts = launchMotor.getCurrentPosition();
-                formerTime = System.currentTimeMillis();
-                telemetry.addLine("Counts" + String.valueOf(counts));
-                telemetry.addLine("Time " + String.valueOf(time));
-                final long speed = 1000 * counts / time;
-                telemetry.addLine("Speed Seconds " + String.valueOf(speed));
-                telemetry.addLine("Power " + String.valueOf(flywheelPower));
-                telemetry.update();
+//            if (time > 250) {
+//                final int counts = launchMotor.getCurrentPosition() - formerCounts;
+//                formerCounts = launchMotor.getCurrentPosition();
+//                formerTime = System.currentTimeMillis();
+//                telemetry.addLine("Counts" + String.valueOf(counts));
+//                telemetry.addLine("Time " + String.valueOf(time));
+//                final long speed = 1000 * counts / time;
+//                telemetry.addLine("Speed Seconds " + String.valueOf(speed));
+//                telemetry.addLine("Power " + String.valueOf(flywheelPower));
+//                telemetry.update();
 
-                if (speed > -1200 && flywheelPower > -0.75) {
-                    //tweak up
-                    flywheelPower = flywheelPower - 0.1;
-                } else if (speed < -1200 && flywheelPower < 0) {
-                    //tweak down
-                    flywheelPower = flywheelPower + 0.75;
-                }
+//                if (speed > -1200 && flywheelPower > -0.75) {
+//                    //tweak up
+//                    flywheelPower = flywheelPower - 0.1;
+//                } else if (speed < -1200 && flywheelPower < 0) {
+//                    //tweak down
+//                    flywheelPower = flywheelPower + 0.75;
+//                }
+//
+//                if (speed < -1150 && speed > -1250) {
+//                    flywheelAtSpeed = true;
+////                    telemetry.addLine("Flywheel is at speed");
+////                    telemetry.update();
+//                } else {
+//                    flywheelAtSpeed = false;
+////                    telemetry.addLine("Flywheel is not at speed");
+////                    telemetry.update();
+//                }
 
-                if (speed < -1150 && speed > -1250) {
-                    flywheelAtSpeed = true;
-//                    telemetry.addLine("Flywheel is at speed");
-//                    telemetry.update();
-                } else {
-                    flywheelAtSpeed = false;
-//                    telemetry.addLine("Flywheel is not at speed");
-//                    telemetry.update();
-                }
+//            }
+//
+//
+//        if (on){
+//            launchMotor.setPower(flywheelPower);
+//        }
+//        else {
+//            launchMotor.setPower(0);
+//        }
 
-            }
-
-
+        flywheelAtSpeed = true;
         if (on){
-            launchMotor.setPower(flywheelPower);
+            launchMotor.setPower(-0.75);
         }
         else {
             launchMotor.setPower(0);
         }
-
     }
     public void push(float on) {
     pushServo.setPosition(on);
