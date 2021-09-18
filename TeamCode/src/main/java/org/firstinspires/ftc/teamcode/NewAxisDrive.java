7e5cfece0453cf94a70b20b1e02194784f865216
metalworks <github@ftc.rocks>
2021-09-18 14:55:43 -0400

    Made a new drive code for new season

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewAxisDrive.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "New Axis Drive")
+
+public class NewAxisDrive extends OpMode {
+    Wheels wheels;
+    private Arm arm;
+    Intake intake;
+    DcMotor leftFrontMotor, leftRearMotor, rightFrontMotor, rightRearMotor;
+
+    @Override
+    public void init() {
+        wheels = new Wheels(hardwareMap, telemetry);
+         arm = new Arm(hardwareMap,telemetry);
+        intake = new Intake(hardwareMap,telemetry);
+        intake.pushServo.setPosition(0);
+    }
+
+    @Override
+    public void loop() {
+
+        if (gamepad1.left_bumper) {
+            wheels.reversePower(gamepad1.left_stick_x, gamepad1.left_stick_y, gamepad1.right_stick_x);
+        }
+        else {
+            if (gamepad1.right_bumper) {
+                wheels.driveCartesian(gamepad1.left_stick_x * 1, gamepad1.left_stick_y * 1, gamepad1.right_stick_x * 1);
+            }
+            else {
+                wheels.driveCartesian(gamepad1.left_stick_x*0.5, gamepad1.left_stick_y*0.5, gamepad1.right_stick_x*0.5);
+            }
+        }
+    }
+
+}
